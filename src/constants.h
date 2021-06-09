f461afa1520fa75aba46ec38d92ef4c79aaff3b2
Sami ELATRI <samielatri.pro@gmail.com>
2021-06-09 04:27:24 +0200

    Update constants.h

--- src/constants.h
+++ src/constants.h
@@ -1,3 +1,8 @@
+/* constants.h */
+
+#ifndef _CONSTANTS_H_
+#define _CONSTANTS_H_
+
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
@@ -5,3 +10,5 @@
 
 #define FPS 30
 #define FRAME_TIME_LENGTH (1000 / FPS)
+
+#endif  /* _CONSTANTS_H_ */
